7d14439a5d3bd50a9cfde54cefc04c884e5063d7
Matthew VanScoik <vanscoik100@gmail.com>
2020-08-27 11:50:16 -0400

    DbContext defined, Connection string added, Attributes added to Data Model

--- GeneralStoreAPI_Demo/Models/Transaction.cs
+++ GeneralStoreAPI_Demo/Models/Transaction.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +9,17 @@ namespace GeneralStoreAPI_Demo.Models
 {
     public class Transaction
     {
+        [Key]
         public int Id { get; set; }
+        [Required] //Database will not allow a save if there is nothing for a required property.
+        [ForeignKey("Customer")] //Links to Customer class. But Db entity will be called "Customer Id"
         public int CustomerId { get; set; }
         public virtual Customer Customer { get; set; }
-        public int ProductId { get; set; }
+        [Required]
+        [ForeignKey(nameof(Product))]
+        public int ProductSKU { get; set; }
         public virtual Product Product { get; set; }
+        [Required]
         public int ItemCount { get; set; }
         public DateTime DateOfTransaction { get; set; }
     }
